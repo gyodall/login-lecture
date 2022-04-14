51b9c060239336ef526c0f5b8d89939c9492f73c
ming <ming@mingui-MacBookAir.local>
2022-04-15 00:32:35 +0900

    소스코드 scr폴더로 분류

--- src/routes/home/home.ctrl.js
+++ src/routes/home/home.ctrl.js
@@ -1,3 +1,13 @@
+"use strict";
+
 const hello = (req,res) => {
     res.render("home/index");
+};
+const login = (req, res) => {
+    res.render("home/login")
+}
+
+module.exports = {
+    hello,
+    login,
 };
